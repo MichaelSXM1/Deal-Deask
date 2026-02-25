5dd8141b89b56e2b37cd80971860f238aaaad7d2
Michael and Moughames <michaelmoughames@gmail.com>
2026-02-25 12:26:04 -0500

    Remove stacked strategy from standard deals form

--- components/dashboard-table.tsx
+++ components/dashboard-table.tsx
@@ -674,7 +674,6 @@ function DealFormModal({
                 <option value="Cash">Cash</option>
                 <option value="Seller Finance">Seller Finance</option>
                 <option value="Subto">Subto</option>
-                <option value="Stacked">Stacked</option>
               </select>
             </Field>
 
